eb7359d6c103d2b4082796758bd0e08a72470820
Hesham AbdAllah <me@heshamabdallah.com>
2023-10-26 06:01:59 +0300

    feat: prefix tenants endpoints with api

--- app/Providers/TenancyServiceProvider.php
+++ app/Providers/TenancyServiceProvider.php
@@ -122,6 +122,7 @@ class TenancyServiceProvider extends ServiceProvider
     {
         if (file_exists(base_path('routes/tenant.php'))) {
             Route::namespace(static::$controllerNamespace)
+                ->prefix('api')
                 ->group(base_path('routes/tenant.php'));
         }
     }
